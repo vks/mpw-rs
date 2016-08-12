b71fab8822605d148ab88879a7c30896ecd41485
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2016-08-12 11:52:35 +0200

    Implement CLI interface
    Parse arguments and read in password.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,112 @@
 
 #[macro_use]
 extern crate lazy_static;
+extern crate clap;
+extern crate rpassword;
+
+use std::io::Write;
+
+use clap::{Arg, App};
+use rpassword::read_password;
 
 mod algorithm;
 use algorithm::*;
 
+static TYPE_HELP: &'static str =
+"The password's template (defaults to 'long' for password, 'name' for login)
+
+x, max, maximum   20 characters, contains symbols.{n}\
+l, long           Copy-friendly, 14 characters, contains symbols.{n}\
+m, med, medium    Copy-friendly, 8 characters, contains symbols.{n}\
+b, basic          8 characters, no symbols.{n}\
+s, short          Copy-friendly, 4 characters, no symbols.{n}\
+i, pin            4 numbers.{n}\
+n, name           9 letter name.{n}\
+p, phrase         20 character sentence.";
+
 fn main() {
+    let matches = App::new("Master Password")
+        .about("A stateless password management solution.")
+        .arg(Arg::with_name("full name")
+             .short("u")
+             .help("The full name of the user")
+             .takes_value(true))
+        .arg(Arg::with_name("type")
+             .short("t")
+             .help(TYPE_HELP)
+             .next_line_help(true)
+             .takes_value(true)
+             .number_of_values(1)
+             .possible_values(&[
+                 "x", "max", "maximum",
+                 "l", "long",
+                 "m", "med", "medium",
+                 "b", "basic",
+                 "s", "short",
+                 "i", "pin",
+                 "n", "name",
+                 "p", "phrase",
+             ]))
+        .arg(Arg::with_name("counter")
+             .short("c")
+             .help("The value of the site counter")
+             .takes_value(true)
+             .number_of_values(1))
+        .arg(Arg::with_name("variant")
+             .short("v")
+             .help("The kind of content to generate")
+             .takes_value(true)
+             .number_of_values(1)
+             .possible_values(&[
+                "p", "password",
+                "l", "login",
+                "a", "answer"
+             ]))
+        .arg(Arg::with_name("context")
+             .short("C")
+             .help("Empty for a universal site or the most significant word(s) of the question")
+             .takes_value(true))
+        .get_matches();
+
+    let full_name = matches.value_of("full name").unwrap_or("");
+
+    let ty = matches.value_of("type").map(|s| match s {
+        "x" | "max" | "maximum"
+            => SiteType::GeneratedMaximum,
+        "l" | "long"
+            => SiteType::GeneratedLong,
+        "m" | "med" | "medium"
+            => SiteType::GeneratedMedium,
+        "b" | "basic"
+            => SiteType::GeneratedBasic,
+        "s" | "short"
+            => SiteType::GeneratedShort,
+        "i" | "pin"
+            => SiteType::GeneratedPIN,
+        "n" | "name"
+            => SiteType::GeneratedName,
+        "p" | "phrase"
+            => SiteType::GeneratedPhrase,
+        _ => panic!("invalid password type"),
+    }).unwrap_or(SiteType::GeneratedLong);
+
+    let counter: u32 = matches.value_of("counter")
+        .map(|c| c.parse().expect("counter must be an unsigned 32-bit integer"))
+        .unwrap_or(1);
+
+    let variant = matches.value_of("variant").map(|s| match s {
+        "p" | "password"
+            => SiteVariant::Password,
+        "l" | "login"
+            => SiteVariant::Login,
+        "a" | "answer"
+            => SiteVariant::Answer,
+        _ => panic!("invalid site variant"),
+    }).unwrap_or(SiteVariant::Password);
+
+    let context = matches.value_of("context").unwrap_or("");
+
+    print!("Please enter the password: ");
+    std::io::stdout().flush().unwrap();
+    let password = read_password().expect("could not read password");
 }
